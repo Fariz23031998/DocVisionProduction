bdaf3bfb7334a7e8da59f5a447b4e2d61fa122ea
Fariz23031998 <xuramovf1888@gmail.com>
2025-11-03 21:53:10 +0500

    Auto1

--- src/main.py
+++ src/main.py
@@ -26,24 +26,24 @@ app.add_middleware(
 )
 
 # Production
-# app.include_router(auth.router, prefix="/v1")
-# app.include_router(users.router, prefix="/v1")
-# app.include_router(tokens.router, prefix="/v1")
-# app.include_router(billing.router, prefix="/v1")
-# app.include_router(regos_proxy.router, prefix="/v1")
-# app.include_router(ai.router, prefix="/v1")
-# app.include_router(system.router, prefix="/v1")
-# app.include_router(click.router, prefix="")
+app.include_router(auth.router, prefix="/v1")
+app.include_router(users.router, prefix="/v1")
+app.include_router(tokens.router, prefix="/v1")
+app.include_router(billing.router, prefix="/v1")
+app.include_router(regos_proxy.router, prefix="/v1")
+app.include_router(ai.router, prefix="/v1")
+app.include_router(system.router, prefix="/v1")
+app.include_router(click.router, prefix="")
 
 # Development
-app.include_router(auth.router, prefix="/api/v1")
-app.include_router(users.router, prefix="/api/v1")
-app.include_router(tokens.router, prefix="/api/v1")
-app.include_router(billing.router, prefix="/api/v1")
-app.include_router(regos_proxy.router, prefix="/api/v1")
-app.include_router(ai.router, prefix="/api/v1")
-app.include_router(system.router, prefix="/api/v1")
-app.include_router(click.router, prefix="/api")
+# app.include_router(auth.router, prefix="/api/v1")
+# app.include_router(users.router, prefix="/api/v1")
+# app.include_router(tokens.router, prefix="/api/v1")
+# app.include_router(billing.router, prefix="/api/v1")
+# app.include_router(regos_proxy.router, prefix="/api/v1")
+# app.include_router(ai.router, prefix="/api/v1")
+# app.include_router(system.router, prefix="/api/v1")
+# app.include_router(click.router, prefix="/api")
 
 
 if __name__ == "__main__":
