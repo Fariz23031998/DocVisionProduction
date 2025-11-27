8f00f2780c04871d8231e1a1108f233d97f2551a
Fariz23031998 <xuramovf1888@gmail.com>
2025-11-27 09:49:15 +0500

    Added translations tj.json

--- src/api/v1/routes/lang.py
+++ src/api/v1/routes/lang.py
@@ -10,14 +10,14 @@ translator = Translator()
 
 @router.get("/{lang_code}")
 async def get_language(lang_code: str):
-    if lang_code.lower() not in ("en", "ru", "uz"):
+    if lang_code.lower() not in ("en", "ru", "uz", "tj"):
         lang_code = "en"
 
     return translator.get_language_translations(lang_code)
 
 @router.get("/{lang_code}/version")
 async def get_version(lang_code: str):
-    if lang_code.lower() not in ("en", "ru", "uz"):
+    if lang_code.lower() not in ("en", "ru", "uz", "tj"):
         lang_code = "en"
 
     return translator.get_language_version(lang_code)
